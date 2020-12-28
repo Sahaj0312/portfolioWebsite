d6ef4ddf95cbc0c16ba61c67863b40b1e6b8649f
sahaj <sahaj0091@gmail.com>
2020-12-28 21:28:22 +0400

    made some color changes

--- app.js
+++ app.js
@@ -25,3 +25,30 @@ var button = document.getElementById("coolBeans");
 button.addEventListener("click", function () {
   darkMode();
 });
+
+const text1 = document.querySelector(".fancy");
+const strText1 = text1.textContent;
+const splitText1 = strText1.split("");
+text1.textContent = "";
+
+for (let i = 0; i < splitText1.length; i++) {
+  text1.innerHTML += "<span>" + splitText1[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  let span = text1.querySelectorAll(`span`)[char];
+  span.classList.add(`fade`);
+  char++;
+  if (char === text1.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
